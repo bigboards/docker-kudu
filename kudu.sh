30fb26f562ac4395f61b7923b479d76f7f9dc64c
Daan Gerits <daan.gerits@gmail.com>
2015-10-08 21:56:27 +0000

    typo: forgot the 'in' when using a case statement in bash

--- kudu.sh
+++ kudu.sh
@@ -6,7 +6,7 @@ export FLAGS_log_dir=/var/log/kudu
 
 [ -z $KUDU_CONF ] && echo "No KUDU_CONF environment variable found! I don't know where to get the kudu configuration" && exit 1
 
-case $1
+case $1 in
 	"master")
 		EXEC=/usr/sbin/kudu-master
 		FLAGS_rpc_bind_addresses=0.0.0.0:7051
