34aaeb4cbbf6fd42adc184e0c81cbeec307ce440
Daan Gerits <daan.gerits@gmail.com>
2015-10-08 18:51:11 +0000

    updated the shell script to take a KUDU_CONF environment variable pointing to the kudu configuration file

--- kudu.sh
+++ kudu.sh
@@ -5,18 +5,18 @@ DEAMON=$1
 export FLAGS_log_dir=/var/log/kudu
 export FLAGS_rpc_bind_addresses=0.0.0.0:7051
 
+[ -z $KUDU_CONF ] && echo "No KUDU_CONF environment variable found! I don't know where to get the kudu configuration" && exit 1
+
 case $1
 	"master")
 		EXEC=/usr/sbin/kudu-master
-		CONF=/etc/kudu/conf/master.gflagfile
 		;;
 	"tserver")
 		EXEC=/usr/sbin/kudu-tserver
-		CONF=/etc/kudu/conf/tserver.gflagfile
 		;;
 	*)
 		echo "Unknown daemon '$1'" && exit 1
 		;;
 esac
 
-${EXEC} --flagfile="${CONF}"
+${EXEC} --flagfile="${KUDU_CONF}"
