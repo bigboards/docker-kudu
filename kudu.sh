b4eae89ddfd9f00df460c8725a10a9012d28799b
Daan Gerits <daan.gerits@gmail.com>
2015-10-08 18:57:28 +0000

    changed the bind addresses for the servers

--- kudu.sh
+++ kudu.sh
@@ -3,16 +3,17 @@
 DEAMON=$1
 
 export FLAGS_log_dir=/var/log/kudu
-export FLAGS_rpc_bind_addresses=0.0.0.0:7051
 
 [ -z $KUDU_CONF ] && echo "No KUDU_CONF environment variable found! I don't know where to get the kudu configuration" && exit 1
 
 case $1
 	"master")
 		EXEC=/usr/sbin/kudu-master
+		FLAGS_rpc_bind_addresses=0.0.0.0:7051
 		;;
 	"tserver")
 		EXEC=/usr/sbin/kudu-tserver
+		FLAGS_rpc_bind_addresses=0.0.0.0:7050
 		;;
 	*)
 		echo "Unknown daemon '$1'" && exit 1
